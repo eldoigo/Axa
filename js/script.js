85fa76d5d613c0ac5cddf8aa0fec5c424cfa3f8a
eldoigo <33632968+eldoigo@users.noreply.github.com>
2018-04-08 00:56:38 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+$(document).ready(function () {
+   'use strict';
+	$('header').fadeIn(2000).removeClass('hidden');
+	$('.container').fadeIn(3000).removeClass('hidden');
+	$('.box1').fadeIn(3000).removeClass('hidden');
+	$('.box2').fadeIn(4000).removeClass('hidden');
+	$('.box3').fadeIn(4500).removeClass('hidden');
+});
