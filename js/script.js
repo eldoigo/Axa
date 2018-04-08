ae2d536197f79845b87e6506ae5b5eeeaf2c0016
J. Doig <33632968+eldoigo@users.noreply.github.com>
2018-04-08 02:38:00 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@
 $(document).ready(function () {
    'use strict';
 	$('header').fadeIn(2000).removeClass('hidden');
-	$('.container').fadeIn(3000).removeClass('hidden');
-	$('.box1').fadeIn(3000).removeClass('hidden');
-	$('.box2').fadeIn(4000).removeClass('hidden');
-	$('.box3').fadeIn(4500).removeClass('hidden');
+	$('.box1').fadeIn(2000).removeClass('hidden');
+	$('.box2').fadeIn(3000).removeClass('hidden');
+	$('.box3').fadeIn(4000).removeClass('hidden');
 });
